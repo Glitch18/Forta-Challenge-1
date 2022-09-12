31e1c04f108b9d4a6f2ed9f662a6b327b6799d89
Glitch18 <atharvakn@gmail.com>
2022-09-12 23:13:35 +0530

    feat: Add bot implementation

---
+++ src/agent.ts
@@ -0,0 +1,44 @@
+import { Finding, HandleTransaction, TransactionEvent, FindingSeverity, FindingType } from "forta-agent";
+import { FORTA_ADDRESS, CREATE_AGENT_FUNCTION, NETHERMIND_ADDRESS } from "./constants";
+
+function provideHandleTransaction(
+  agentFunctionAbi: string,
+  fortaContractAddress: string,
+  nethermindDeployAddress: string
+): HandleTransaction {
+  return async (txEvent: TransactionEvent) => {
+    const findings: Finding[] = [];
+
+    // filter the transaction logs for createAgent functions from Forta contract
+    const tetherTransferEvents = txEvent.filterFunction(agentFunctionAbi, fortaContractAddress);
+
+    tetherTransferEvents.forEach((transferEvent) => {
+      // Extract arguments from transaction
+      const { agentId, metadata, chainIds, owner } = transferEvent.args;
+
+      // Check if Bot was deployed by Nethermind
+      if (owner === nethermindDeployAddress) {
+        findings.push(
+          Finding.fromObject({
+            name: "Nethermind Bot Deployment",
+            description: "Bot deployed from Nethermind",
+            alertId: "NTRMND-1",
+            severity: FindingSeverity.Low,
+            type: FindingType.Info,
+            metadata: {
+              agentId: agentId,
+              metadata: metadata,
+              chainIds: chainIds,
+            },
+          })
+        );
+      }
+    });
+
+    return findings;
+  };
+}
+
+export default {
+  handleTransaction: provideHandleTransaction(CREATE_AGENT_FUNCTION, FORTA_ADDRESS, NETHERMIND_ADDRESS),
+};
---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const CREATE_AGENT_FUNCTION =
+  "function createAgent(uint256 agentId, address owner, string metadata, uint256[] chainIds)";
+export const NETHERMIND_ADDRESS = "0x88dC3a2284FA62e0027d6D6B1fCfDd2141a143b8";
+export const FORTA_ADDRESS = "0x61447385B019187daa48e91c55c02AF1F1f3F863";
